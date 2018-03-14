12eddecac3cfa1cdcf1e3603c1de0f808a98cdfc
daniel-certa-1228 <mfdancerta@gmail.com>
2018-03-14 17:22:47 -0500

    in the middle of operators

---
+++ operators.js
@@ -0,0 +1,17 @@
+// Bitwise Operators
+var A = 2;
+var B = 3;
+var result_1 = A & B; // bitwise AND
+console.log(result_1);
+var result_2 = A | B; //bitwise OR
+console.log(result_2);
+var result_3 = A ^ B; //bitwise XOR
+console.log(result_3);
+var result_4 = (~B); //bitwise NOT
+console.log(result_4);
+var result_5 = (A << 1); //left shift
+console.log(result_5);
+var result_6 = (A >> 1); //right shift
+console.log(result_6);
+var result_7 = (A >>> 1); //Right shift with Zero
+console.log(result_7);
